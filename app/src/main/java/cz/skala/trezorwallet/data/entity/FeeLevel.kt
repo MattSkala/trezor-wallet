cf36b371ff0d8791367898e8577c611a373cc113
Matouš Skála <skala.matous@gmail.com>
2018-11-09 16:31:32 +0100

    update fee levels

--- app/src/main/java/cz/skala/trezorwallet/data/entity/FeeLevel.kt
+++ app/src/main/java/cz/skala/trezorwallet/data/entity/FeeLevel.kt
@@ -3,8 +3,8 @@ package cz.skala.trezorwallet.data.entity
 import cz.skala.trezorwallet.R
 
 enum class FeeLevel(val titleRes: Int, val blocks: Int) {
-    HIGH(R.string.fee_high, 2),
-    NORMAL(R.string.fee_normal, 6),
-    ECONOMY(R.string.fee_economy, 25),
-    LOW(R.string.fee_low, 50) // 25 is max, so LOW = ECONOMY/2
+    HIGH(R.string.fee_high, 2), // asap
+    NORMAL(R.string.fee_normal, 6), // 1 hour
+    ECONOMY(R.string.fee_economy, 36), // 6 hours
+    LOW(R.string.fee_low, 432) // 3 days
 }
